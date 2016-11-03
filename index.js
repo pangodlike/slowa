c6a5575df819181c87bd38489c3c37a6c14b6165
Ian Fillion-de Kiewit <ianfdk@gmail.com>
2016-11-03 01:53:44 -0400

    Tests pass

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const MongoDBAdapter = require('./src/db');
+const MongoDBAdapter = require('./src/db/mongodb_adapter.js');
 const Sabot = require('./src/sabot.js');
 const config = require('./config.js');
 
 // Initialize bot
-dbAdapter = new MongoDBAdapter(config.mongoDBURL);
+dbAdapter = new MongoDBAdapter({ url: config.mongoDBURL });
 let bot = new Sabot(dbAdapter);
 bot.login(config.discordToken);
 
--- src/sabot.js
+++ src/sabot.js
@@ -3,26 +3,59 @@ const discord = require('discord.js');
 class Sabot {
   constructor(dbAdapter) {
     this.dbAdapter = dbAdapter;
-
-    this.api = new discord.Client();
-    this.api.on('ready', () => { this.handleReady(); });
-    this.api.on('message', (message) => { this.handleMessage(message); });
   }
 
   login(token) {
-    console.log('Sabot logging in...')
-    this.api.login(token);
+    this.client = this.makeDiscordClient();
+    this.client.once('ready', () => {this.handleReady()});
+    this.client.on('message', (message) => {this.handleMessage(message)});
+    this.client.login(token);
     this.dbAdapter.connect();
   }
 
   logout() {
-    console.log('Sabot logging out...')
-    this.api.destroy();
+    this.client.destroy();
+  }
+
+  processMessage(message) {
+    let content = message.content;
+    // Split message's text into words
+    let words = content.split(/\s+\W+|\s+|\W+/);
+    // Count how many times each word appears
+    let counter = {};
+    words.forEach((word) => {
+      if (word.length === 0) {
+        return;
+      }
+      if (counter.hasOwnProperty(word)) {
+        counter[word]++;
+      } else {
+        counter[word] = 1;
+      }
+    });
+    // Increment every word's count in the DB
+    for (let word in counter) {
+      this.dbAdapter.incrementWord(word, counter[word]);
+    }
+  }
+
+  processWordCountCommand(message) {
+    let spelling = message.content.split(' ')[1];
+    return this.dbAdapter.getWordCount(spelling).then(
+      (res) => {
+        console.log(res)
+        if (res === null) {
+          message.channel.sendMessage(`Word "${spelling}" has never been spotted here...`);
+        } else {
+          message.channel.sendMessage(`Word "${spelling}" has been spotted ${res.count} times here!`);
+        }
+    });
   }
 
   // When the bot arrives online
   handleReady() {
-    console.log('Sabot ready!');
+    console.log('ready')
+    // Maybe say hello or something
   }
 
   // When a message is posted in a channel
@@ -32,22 +65,14 @@ class Sabot {
       return;
     }
     if (content.startsWith('!word_count')) {
-      let word = content.split(' ')[1]
-      this.dbAdapter.getWordCount(word).then(
-        (res) => {
-          if (res === null) {
-            message.channel.sendMessage(`Word "${word}" has never been spotted here...`);
-          } else {
-            message.channel.sendMessage(`Word "${word}" has been spotted ${res.count} times here!`);
-          }
-        });
+      this.processWordCountCommand(message);
     } else {
-      this.dbAdapter.incrementWord(message, 1);
+      this.processMessage(message);
     }
   }
 
-  splitCommand(content) {
-    return content.split(' ');
+  makeDiscordClient() {
+    return new discord.Client();
   }
 }
 
