0490cfbc2e5d8f20b378520289a83a9d4b77ae0e
pangodlike <40629632+pangodlike@users.noreply.github.com>
2018-07-08 12:26:28 +0200

    Update and rename config_example.js to config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+let config = {};
+
+// Authorize URL: https://discordapp.com/oauth2/authorize?client_id=465463356573351947&scope=bot&permissions=0
+config.discordToken = 'NDY1NDYzMzU2NTczMzUxOTQ3.DiN4EQ.w6Ad3OpxzzuJFg9uCP4B5HSjPVY';
+
+
+module.exports = config;
